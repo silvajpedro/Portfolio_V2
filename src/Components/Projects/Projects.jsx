8d4defc263c12193c5d6134dc5662ac7fe8e7a41
silvajpedro <silvajpro.31@gmail.com>
2024-01-07 17:04:59 -0300

    prismic configs

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,28 +3,51 @@ import * as S from "./Projects_style.jsx";
 import Carousel from "./Carousel/Carousel.jsx";
 import { Projects_Carousel } from "./Carousel/Carousel_Assets";
 import Phone from "./Contact/phone.jsx";
+
+// prismic config
+import * as Prismic from "@prismicio/client";
+import { useFirstPrismicDocument } from "@prismicio/react";
+
+export const client = Prismic.createClient("silvajpedro", {
+  accessToken:
+    "MC5aWlZoU3hBQUFDQUFqaDll.B--_vUDvv701QnUS77-977-977-977-9Y--_vWod77-9fjda77-9L--_vS7vv71dau-_ve-_vUIn77-9",
+});
+
 export default function Projects() {
+  const [document] = useFirstPrismicDocument();
+
+  console.log(document);
+
+  if (!document) {
+    return <div>Carregando...</div>;
+  }
+
   return (
     <AlignBox>
       <ContentBox SizeH="235vh">
+        
         <S.CarroselBox>
-        <h2>Projetos</h2>
-        <Carousel>
-    {/* a caixa figure que vai alinhar cada um no seu quadrado 
+          <h2>Projetos</h2>
+
+          <Carousel>
+            
+            {/* a caixa figure que vai alinhar cada um no seu quadrado 
     verificar o estilo e tentar diminuir o máximo de linhas possível
     arquivo carousel.jsx e project style.jsx */}
-          {Projects_Carousel.map((item)=>(
-            <figure>
-              <a href={item.link} target="_blank">
-           <img src={item.projeto} alt="imagem dos projetos do joao"/>
-            </a>
-           <figcaption>{item.descricao_projeto}</figcaption>
-            </figure> 
-          ))}
-        </Carousel>
 
-          </S.CarroselBox>
-          <Phone/>
+            {document.data.body[0].items.map((item, id) => (
+              <figure key={id}>
+                <a href={item.link.url} target="_blank">
+                  <img src={item.imagem_projeto.url} alt="imagem dos projetos do joao" />
+                </a>
+                <figcaption>{item.descricao_projeto[0].text}</figcaption>
+              </figure>
+            ))}
+          </Carousel>
+        </S.CarroselBox>
+
+        <Phone />
+        
       </ContentBox>
     </AlignBox>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {client} from "./Components/Projects/Projects.jsx";
+import { PrismicProvider } from '@prismicio/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App /> 
+    <PrismicProvider client={client}>
+        <App /> 
+    </PrismicProvider>
 )
